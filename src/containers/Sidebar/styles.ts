e10647814ea6906bee5dd0ed7fe5928f0b4ddaf5
goescaroline <goes_caroline@hotmail.com>
2023-03-06 14:48:03 -0300

    fazendo alterações na sessao projetos

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -8,9 +8,9 @@ export const Descricao = styled(P)`
 export const BotaoTema = styled.button`
   font-size: 10px;
   border-radius: 12px;
-  color: #eee;
+  color: ${(props) => props.theme.corDeFundo};
   font-weight: bold;
-  background-color: #282a35;
+  background-color: ${(props) => props.theme.corPrincipal};
   padding: 8px;
   cursor: pointer;
 `
@@ -18,4 +18,9 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
